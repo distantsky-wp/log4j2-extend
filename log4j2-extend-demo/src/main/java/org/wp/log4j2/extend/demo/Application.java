a833dafe748bafe42b28a64a2e1cddd052da4b77
wangpeng <distantsky_wp@outlook.com>
2024-05-01 15:26:35 +0800

    调整基本架构

---
+++ log4j2-extend-demo/src/main/java/org/wp/log4j2/extend/demo/Application.java
@@ -0,0 +1,20 @@
+/*
+ * Copyright © 2024 By wang-p,All rights reserved.
+ */
+package org.wp.log4j2.extend.demo;
+
+/**
+ * @class Application
+ * @desc
+ * @author wang-p
+ * @version 1.0.0
+ * @date 2024/5/1
+ * @modify
+ */
+public class Application {
+
+    public static void main(String[] args) {
+
+    }
+
+}
--- src/test/java/org/wp/log4j2/extend/Tests.java
+++ src/test/java/org/wp/log4j2/extend/Tests.java
@@ -1,15 +1,15 @@
 /*
  * Copyright © 2024 By wang-p,All rights reserved.
  */
-package org.wp.log.extend;
+package org.wp.log4j2.extend;
 
 /**
- * @class: Test
- * @desc:
- * @author: wang-p
- * @date: 2024/5/1
- * @version: 1.0.0
- * @modify:
+ * @class Test
+ * @desc
+ * @author wang-p
+ * @date 2024/5/1
+ * @version 1.0.0
+ * @modify
  */
 public class Tests {
 
