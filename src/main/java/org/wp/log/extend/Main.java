fbe72e94bdbc86f835018b26a67735c8dfc86e2a
wangpeng <distantsky_wp@outlook.com>
2024-05-01 12:44:35 +0800

    工程初始化

---
+++ src/main/java/org/wp/log/extend/Main.java
@@ -0,0 +1,15 @@
+/*
+ * Copyright © 2024 By wang-p,All rights reserved.
+ */
+package org.wp.log.extend;
+
+/**
+ * @class: Main
+ * @desc:
+ * @author: wang-p
+ * @date: 2024/5/1
+ * @version: 1.0.0
+ * @modify:
+ */
+public class Main {
+}
