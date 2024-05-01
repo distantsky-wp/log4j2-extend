1f3362ac8768275aaf3b2d60fb11fef62328c2f8
wangpeng <distantsky_wp@outlook.com>
2024-05-01 19:46:02 +0800

    demo基本搭建完成

--- log4j2-extend-demo/src/main/java/org/wp/log4j2/extend/demo/Application.java
+++ log4j2-extend-demo/src/main/java/org/wp/log4j2/extend/demo/Application.java
@@ -3,18 +3,29 @@
  */
 package org.wp.log4j2.extend.demo;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 /**
- * @class Application
- * @desc
  * @author wang-p
  * @version 1.0.0
+ * @class Application
+ * @desc
  * @date 2024/5/1
  * @modify
  */
+@SpringBootApplication
 public class Application {
 
     public static void main(String[] args) {
+        shutdownHook();
+        SpringApplication.run(Application.class, args);
+    }
 
+    private static void shutdownHook() {
+        Runtime.getRuntime().addShutdownHook(new Thread(() -> {
+
+        }));
     }
 
 }
